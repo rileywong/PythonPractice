da58b1f3080aa51165f8f16e71110caa9b55716c
Riley Wong <YungReezy@Rileys-MacBook-Air.local>
2017-09-26 19:29:17 -0400

    Update

--- Tourism2/tourism2.py
+++ Tourism2/tourism2.py
@@ -11,6 +11,62 @@ def printMinimized(people,locations,preferences,duration,openTime,closeTime,pref
 	print("preferer : " + str(preferer))
 	print("preferences : " + str(preference ))
 
+def getPreferences(person,preferer,preference):
+	prefSequence = []
+	for x in range(len(preferer)):
+		if preferer[x] == person:
+			prefSequence.append(preference[x])
+	return prefSequence
+
+def getVisitedPlaces(sequence,duration,openTime,closeTime):
+	timeFrame = []
+
+	minTime = min(openTime)
+	maxTime = max(closeTime)
+
+	for x in range(minTime,maxTime):
+		timeFrame.append(x)
+
+	stop = 0
+	placesVisited = []
+
+	for x in range(len(sequence)):
+		if stop == 1:
+			break
+		index = openTime[sequence[x]] % minTime
+		visitDuration = duration[sequence[x]]
+		
+		for y in range(openTime[sequence[x]],closeTime[sequence[x]]):
+			# print("THIS IS FOR TIME + " + str(sequence[x]))
+			# print(timeFrame)
+			if timeFrame[y%openTime[sequence[x]] + (openTime[sequence[x]]-minTime)] != -1:
+
+				visitDuration = visitDuration-1
+				timeFrame[y%openTime[sequence[x]]+ (openTime[sequence[x]]-minTime)] = -1
+				# print("AFTER THE CHANGE " + str(timeFrame)) 
+				if visitDuration == 0:
+					break
+			else:
+				next
+
+		if visitDuration != 0:
+			stop = 1
+		else:
+			placesVisited.append(sequence[x])
+			
+	return placesVisited
+
+def getSatisfaction(prefPP,visitedPlaces,locations):
+	numSatis = 0
+	for x in prefPP:
+		if x in visitedPlaces:
+			numSatis = numSatis +1
+
+	if numSatis == len(prefPP):
+		numSatis = locations
+
+	return numSatis
+
 def main():
 
 	f = open(sys.argv[1], 'r')
@@ -47,10 +103,45 @@ def main():
 			preference.append(int(params[1]))
 			continue
 
-	printMinimized(people,locations,preferences,duration,openTime,closeTime,preferer,preference)
+	# printMinimized(people,locations,preferences,duration,openTime,closeTime,preferer,preference)
 
+	preferencePerPerson = []
+	for x in range(people):
+		preferencePerPerson.append(getPreferences(x+1,preferer,preference))
 
+	locationArray = []
+	for x in range(locations):
+		locationArray.append(x)
 
+	
+	minSatisArray = []
+	for items in itertools.permutations(locationArray,len(locationArray)):
+		
+	 	minSats = []
+	 	for prefPP in preferencePerPerson:
+	 		visitedPlace = getVisitedPlaces(items,duration,openTime,closeTime)
+	 		minSats.append(getSatisfaction(prefPP,visitedPlace,locations))
+	 	minSatisArray.append(min(minSats))
+
+	print("satisfaction("+str(max(minSatisArray))+").")
+	# print(getSatisfaction([1,2,3],[6,3,1,2],10))	
+	# print(getSatisfaction([1,2,3],[6],10))				
 
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
